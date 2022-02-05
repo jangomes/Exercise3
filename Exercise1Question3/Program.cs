45336c384316ca46893fe12ee2ee94b0b33f87cc
Janaina Gomes <90685145+jangomes@users.noreply.github.com>
2022-02-05 13:05:43 +0000

    Add files via upload

---
+++ Exercise1Question3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise1Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a total amount of time in minutes only: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            int t = h / 60;
+            Console.WriteLine("It is " + t + " hour(s) and " + h%60 + "  minute(s)");
+            
+
+        }
+    }
+}
